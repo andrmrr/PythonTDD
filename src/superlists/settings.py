eb8c6d3222a341bfb54899f1db4616d24c10d712
Andreja Andrejic <andr.andrejic@gmail.com>
2025-03-19 14:56:38 +0100

    added models in accounts for authentication

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "lists",
-    # "accounts",
+    "accounts",
 ]
 
-# AUTH_USER_MODEL = "accounts.ListUser"
+AUTH_USER_MODEL = "accounts.User"
 AUTHENTICATION_BACKENDS = [
     # "accounts.authentication.PasswordlessAuthenticationBackend",
 ]
